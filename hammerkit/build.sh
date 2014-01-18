2e05433bd179aef849f1866d5264020e3a0704d8
Sergio Rubio <rubiojr@frameos.org>
2014-01-18 19:08:28 +0100

    build script tweaks

--- hammerkit/build.sh
+++ hammerkit/build.sh
@@ -1,5 +1,5 @@
 # Requires libc6-dev:i386 and libssl-dev:i386 gcc-multilib
-MRUBY_CONFIG=`pwd`/hammerkit_config.rb make
+MRUBY_CONFIG=hammerkit/hammerkit_config.rb make
 for f in `ls build/i386/bin/*`; do
 	cp $f bin/`basename $f`-i386
 done
