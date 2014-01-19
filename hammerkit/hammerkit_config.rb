be608055bdc5559c5dc98d82c1977c033238d7d3
Sergio Rubio <rubiojr@frameos.org>
2014-01-19 16:27:31 +0100

    Optionals i386 builds

--- hammerkit/hammerkit_config.rb
+++ hammerkit/hammerkit_config.rb
@@ -16,22 +16,24 @@ MRuby::Build.new do |conf|
 end
 
 # Define cross build settings
-MRuby::CrossBuild.new('i386') do |conf|
-  toolchain :gcc
+if ENV['HAMMERKIT_ARCH_i386']
+  MRuby::CrossBuild.new('i386') do |conf|
+    toolchain :gcc
 
-  conf.cc.flags << "-m32"
-  conf.linker.flags << "-m32"
+    conf.cc.flags << "-m32"
+    conf.linker.flags << "-m32"
 
-  # include the IIJ gembox
-  conf.gembox 'iij'
+    # include the IIJ gembox
+    conf.gembox 'iij'
 
-  conf.gem :git => 'https://github.com/mattn/mruby-updategems.git'
-  conf.gem :git => 'https://github.com/matsumoto-r/mruby-simplehttp'
-  conf.gem :git => 'https://github.com/rubiojr/mruby-getoptlong.git'
-  conf.gem :git => 'https://github.com/mattn/mruby-json.git'
-  conf.gem :git => 'https://github.com/luisbebop/mruby-polarssl.git'
-  conf.gem :git => 'https://github.com/matsumoto-r/mruby-simplehttp'
-  conf.gem :git => 'https://github.com/rubiojr/mruby-shutils'
+    conf.gem :git => 'https://github.com/mattn/mruby-updategems.git'
+    conf.gem :git => 'https://github.com/matsumoto-r/mruby-simplehttp'
+    conf.gem :git => 'https://github.com/rubiojr/mruby-getoptlong.git'
+    conf.gem :git => 'https://github.com/mattn/mruby-json.git'
+    conf.gem :git => 'https://github.com/luisbebop/mruby-polarssl.git'
+    conf.gem :git => 'https://github.com/matsumoto-r/mruby-simplehttp'
+    conf.gem :git => 'https://github.com/rubiojr/mruby-shutils'
 
-  conf.gem :core => 'mruby-hammerkit'
+    conf.gem :core => 'mruby-hammerkit'
+  end
 end
